a91670f74220dee3784a41a320670374b4879e43
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-05-18 20:21:05 +0000

    Add search functionality to vehicles#index

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,10 @@
 class Vehicle < ActiveRecord::Base
+    
+    def self.search(search)
+        if search
+            where('ext_color LIKE ? OR int_color LIKE ? OR lat LIKE ? OR long LIKE ? OR make LIKE ? OR mileage LIKE ? OR model LIKE ? OR vehicle_type LIKE ? OR price LIKE ? OR stocknum LIKE ? OR vin LIKE ? OR year LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+        else
+            where(nil)
+        end
+    end
 end
